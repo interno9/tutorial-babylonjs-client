9f3e735bb083447a9be5541d3174e77e4363f176
Nicola <nicolajurifontana@gmail.com>
2023-08-31 13:34:28 +0200

    app

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
-import Menu from './menu'
+import Menu from "./menu";
 
-window.addEventListener('DOMContentLoaded', () => {
-    // Create the game using the 'renderCanvas'.
-    let menu = new Menu('renderCanvas');
+window.addEventListener("DOMContentLoaded", () => {
+  // Create the game using the 'renderCanvas'.
+  let menu = new Menu("renderCanvas");
 
-    // Create the scene.
-    menu.createMenu();
+  // Create the scene.
+  menu.createMenu();
 });
